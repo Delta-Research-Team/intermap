c9631c2df11886e3f14d76ef36ed0daeaac190b2
Roy González Alemán <roy.gonzalez.aleman@gmail.com>
2024-11-17 11:49:13 +0100

    [NF] Decrease memory footprint by returning unique hbonds
    - Instead of returning a list of all hbonds detected at each frame, returns a dict of unique DHA tuples associated to a list of frames where the hbond is present.

--- src/intermap/runner.py
+++ src/intermap/runner.py
@@ -1,4 +1,5 @@
 # Created by gonzalezroy at 11/14/24
+from collections import defaultdict
 from multiprocessing import Pool
 
 import mdtraj as md
@@ -12,6 +13,7 @@ def run(args):
     """
     Entry point for running the intermap workflow
     """
+    # Parse the arguments
     traj = args.traj
     topo = args.topo
     sel1 = args.sel1
@@ -22,25 +24,38 @@ def run(args):
     # Load the topology and the trajectory
     master_traj = md.load_frame(traj, top=topo, index=0)
 
+    # Get the selections
     seles = iman(sel1, sel2, master_traj)
     s1_donors, s1_hydros, s1_acc = seles.s1_donors, seles.s1_hydros, seles.s1_acc
     s2_donors, s2_hydros, s2_acc = seles.s2_donors, seles.s2_hydros, seles.s2_acc
 
-    # Define the function to be parallelized
-    hbonds = []
+    # Yield chunks of the trajectory
     chunks = tt.get_traj_chunks(topo, [traj], chunk_size=chunk_size)
-    for chunk in chunks:
-        xyz = chunk.xyz
-        with Pool(nprocs) as p:
-            arguments = []
-            for frame in xyz:
-                arguments.append((frame, s1_donors, s1_hydros, s1_acc,
-                                  s2_donors, s2_hydros, s2_acc))
-            del xyz
-            results = p.starmap(find_hbonds, arguments)
-            hbonds.extend(results)
 
+    # Find the hydrogen bonds in parallel for each chunk
+    hbonds = defaultdict(list)
+    for chunk in chunks:
+
+        # Get the chunk coords and delete the chunk object to free memory
+        xyz = chunk.xyz
+        del chunk
+
+        # Prepare the arguments; using a generator for delayed evaluation
+        arguments = (
+            (coord, s1_donors, s1_hydros, s1_acc, s2_donors, s2_hydros, s2_acc)
+            for coord in xyz)
+
+        # Run the function in parallel
+        with Pool(nprocs) as p:
+            hbonds_chunk = p.starmap(find_hbonds, arguments)
+            del xyz
+
+            # Convert the results to a dictionary of counts per frame
+            for frame, hbonds_frame in enumerate(hbonds_chunk):
+                for hbond in hbonds_frame:
+                    hbonds[tuple(hbond)].append(frame)
     return seles, hbonds
+
 
 # =============================================================================
 # Debugging area
@@ -51,6 +66,7 @@ def run(args):
 # traj = '/media/rglez/Expansion/RoyData/oxo-8/raw/water/A1/8oxoGA1_1_sk100.nc'
 # sel1 = "(resname =~ '(5|3)?D([ATGC])|(8OG){1}(3|5)?$')"
 # sel2 = "water"
-# nprocs = 8
-# chunk_size = 150
-# args = Namespace(topo=topo, traj=traj, sel1=sel1, sel2=sel2, nprocs=nprocs, chunk_size=chunk_size)
+# nprocs = 16
+# chunk_size = 100
+# args = Namespace(topo=topo, traj=traj, sel1=sel1, sel2=sel2, nprocs=nprocs,
+#                  chunk_size=chunk_size)
