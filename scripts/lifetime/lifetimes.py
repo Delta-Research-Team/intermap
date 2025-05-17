b941c303da36f4b68dec05103f6cce09969fbb76
Roy González Alemán <roy.gonzalez.aleman@gmail.com>
2025-05-17 17:30:19 +0200

    [WP] Add atom_composition.py for trajectory analysis and update lifetimes.py to use boxplot

--- scripts/lifetime/lifetimes.py
+++ scripts/lifetime/lifetimes.py
@@ -1,9 +1,8 @@
 # Created by gonzalezroy at 5/16/25
-from bitarray import bitarray as ba
-from bitarray import util as bu
-import numpy as np
 import matplotlib.pyplot as plt
+from bitarray import bitarray as ba, util as bu
+
 array = ba('00010101110101011111000010101011011111110010111111010101000111')
-lifetimes = np.mean([x[2] - x[1] for x in bu.intervals(array) if x[0] == 1])
-plt.hist(lifetimes)
+lifetimes = [x[2] - x[1] for x in bu.intervals(array) if x[0] == 1]
+plt.boxplot(lifetimes)
 plt.show()
