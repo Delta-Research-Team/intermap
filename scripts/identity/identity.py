ef0bc8da746b8451b69f9fba8f0a57e3820bae40
Roy González Alemán <roy.gonzalez.aleman@gmail.com>
2025-05-18 23:59:49 +0200

    [WP] Refactor identity.py to streamline data loading and processing; improve handling of file paths and interaction comparisons

--- scripts/identity/identity.py
+++ scripts/identity/identity.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+import os
+from os.path import basename, join
 
 import numpy as np
 import pandas as pd
@@ -8,10 +9,19 @@ from rgpack import generals as gnl
 # =============================================================================
 # User-defined variables
 # =============================================================================
-prolif_pickle = '/media/gonzalezroy/Expansion/RoyData/intermap/correctness/prolif/lig-prot.pkl'
-imap_csv = '/media/gonzalezroy/Expansion/RoyData/intermap/correctness/intermap/imap_lig-prot_InterMap.csv'
-df = gnl.unpickle_from_file(prolif_pickle)
-imap = pd.read_csv(imap_csv)
+root_dir = '/media/rglez/Roy2TB/Dropbox/DockingBoys/storage/identity_prolif_100frames/'
+cases = [join(root_dir, x) for x in os.listdir(root_dir)]
+
+results = {}
+for case in cases:
+    pkl = next(gnl.recursive_finder('fingerprint.pkl', case))
+    csv = next(gnl.recursive_finder('*_full.csv', case))
+    results[basename(case)] = (pkl, csv)
+
+case = 'mpro'
+
+df = gnl.unpickle_from_file(results[case][0]).to_dataframe()
+imap = pd.read_csv(results[case][1], header=1, na_values=('', ' '))
 
 # =============================================================================
 # Transforming Prolif data
@@ -21,25 +31,29 @@ for column in df.columns:
     r1 = column[0].split('.')[0]
     r2 = column[1].split('.')[0]
     inter = column[2]
-    time = np.asarray(df[column])
+    time = np.asarray(df[column], dtype=bool)
     prolif_dict[(r1, r2, inter)] = time
+total_prolif = 0
+for x in prolif_dict:
+    total_prolif += prolif_dict[x].sum()
 
 prolif_inters = set()
 for x in prolif_dict:
     r1, r2, inter = x
-    prolif_inters.add(inter)
+    prolif_inters.add(inter.strip())
 
 # =============================================================================
 # Reducing InterMap data
 # =============================================================================
 imap_dict_raw = dict()
 for x in imap.values:
-    a1, a2, a3, inter_raw, prev, time = x
-    inter = str(inter_raw)
-    a1_resname, a1_resid, a1_atname = a1.split('_')
-    a1_label = f'{a1_resname}{a1_resid}'
-    a2_resname, a2_resid, a2_atname = a2.split('_')
-    a2_label = f'{a2_resname}{a2_resid}'
+    a1, _, a2, _, a3, inter_raw, prev, time = x
+    a1, a2, inter_raw = a1.strip(), a2.strip(), inter_raw.strip()
+    inter = str(inter_raw).strip()
+    a1_resname, a1_resnum, a1_resid = a1.split('_')
+    a1_label = f'{a1_resname}{a1_resnum}'
+    a2_resname, a2_resnum, a2_resid = a2.split('_')
+    a2_label = f'{a2_resname}{a2_resnum}'.strip()
     current_time = ba(time)
 
     if (a1_label, a2_label, inter) in imap_dict_raw:
@@ -52,11 +66,14 @@ for x in imap.values:
 imap_dict = dict()
 for x in imap_dict_raw:
     imap_dict[x] = np.asarray(imap_dict_raw[x].tolist())
+total_imap = 0
+for x in imap_dict:
+    total_imap += imap_dict[x].sum()
 
 imap_inters = set()
 for x in imap_dict_raw:
     r1, r2, inter = x
-    imap_inters.add(inter)
+    imap_inters.add(inter.strip())
 
 # =============================================================================
 # Comparing Prolif and InterMap data in general terms
@@ -71,19 +88,29 @@ not_prolif = imap_inters - prolif_inters
 print('Interactions in InterMap but not in Prolif:', not_prolif)
 
 # PLF @ IMAP
+# todo: what about self-interactions? within same residue? Remove a==b !
+INTER = 'CationPi'
 count = 0
 for pl_inter in prolif_dict:
+    a, b, c = pl_inter
+    pl_inverse = (b, a, c)
+    if (c != INTER) or (a == b):
+        continue
     if pl_inter not in imap_dict:
         count += 1
         print(f'Prolif interaction {pl_inter} not in InterMap')
 print(
-    f'Percentage of Prolif interactions not in InterMap: {count / len(prolif_dict) * 100:.2f}%')
+    f'Percentage of Prolif {INTER} not in InterMap: {count / len(prolif_dict) * 100:.2f}%')
 
 # IMAP @ PLF
 count = 0
 for im_inter in imap_dict:
+    d, e, f = im_inter
+    im_inverse = (e, d, f)
+    if (d == e) or (f != INTER):
+        continue
     if (im_inter not in prolif_dict) and (
-            im_inter[-1] not in ['CloseContact']):
+            im_inter[-1] in ['CloseContact']):
         count += 1
         print(f'InterMap interaction {im_inter} not in Prolif')
 print(
@@ -110,5 +137,7 @@ print(
 #             print(plf_time)
 #             print(imap_time)
 
-gnl.pickle_to_file(imap_dict, '/home/gonzalezroy/RoyHub/intermap/scripts/identity/imap_dict.pkl')
-gnl.pickle_to_file(prolif_dict, '/home/gonzalezroy/RoyHub/intermap/scripts/identity/prolif_dict.pkl')
+gnl.pickle_to_file(imap_dict,
+                   '/home/gonzalezroy/RoyHub/intermap/scripts/identity/imap_dict.pkl')
+gnl.pickle_to_file(prolif_dict,
+                   '/home/gonzalezroy/RoyHub/intermap/scripts/identity/prolif_dict.pkl')
